67a1810d9060fa28f1267e3a4a017b114ab70fc0
Haruka Ichinose <hic21@uclive.ac.nz>
2022-01-17 22:04:58 +1300

    Added config file for serving port and unauthorized access handler to the router.

--- vue-frontend/src/router/index.js
+++ vue-frontend/src/router/index.js
@@ -52,4 +52,16 @@ const router = new VueRouter({
   routes
 });
 
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/login', '/register', '/home'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
 export default router
