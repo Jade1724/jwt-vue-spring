d3e92c6a2ca8674396cc0dd849b594a1f297b8c4
Haruka Ichinose <hic21@uclive.ac.nz>
2022-01-17 15:31:46 +1300

    Imported Vee Validate and bootstrap to main.js.

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VeeValidate from 'vee-validate'
+import 'bootstrap'
+import 'bootstrap/dist/css/'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VeeValidate);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
